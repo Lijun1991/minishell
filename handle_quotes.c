30921a8251b805bd53282f773ec711c867060171
Lijun Wang <wang283808065@gmail.com>
2017-05-07 23:31:37 -0700

    handle quotes, need to work more on while

---
+++ handle_quotes.c
@@ -0,0 +1,119 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_quotes.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lwang <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/07 23:08:17 by lwang             #+#    #+#             */
+/*   Updated: 2017/05/07 23:08:20 by lwang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static void	fill_space_helper(char *line, int start, int end)
+{
+	while (line[start] != '\0' && start <= end)
+	{
+		if (line[start] == ' ')
+			line[start] = 01;
+		else if (line[start] == '\t')
+			line[start] = 02;
+		else if (line[start] == '\v')
+			line[start] = 03;
+		else if (line[start] == '\r')
+			line[start] = 04;
+		start++;
+	}
+}
+
+void	change_space(char *line)
+{
+	int		i;
+	int		start;
+	int		end;
+
+	i = 0;
+	start = 0;
+	end = 0;
+	while (line[i] != '\0')
+	{
+		while (line[i] != '"' && line[i] != '\0')
+			i++;
+		start = i;
+		i++;
+		while (line[i] != '"' && line[i] != '\0')
+			i++;
+		end = i;
+		i++;
+		if (end != 0)
+		{
+			fill_space_helper(line, start, end);
+		}
+	}
+}
+
+void	refill_space(char *av)
+{
+	int i;
+
+	i = 0;
+	while (av[i])
+	{
+		if (av[i] == 01)
+			av[i] = ' ';
+		else if (av[i] == 02)
+			av[i] = '\t';
+		else if (av[i] == 03)
+			av[i] = '\v';
+		else if (av[i] == 04)
+			av[i] = '\r';
+		i++;
+	}
+}
+
+int			line_wordcount(char *str)
+{
+	int		i;
+	int		j;
+
+	i = -1;
+	j = 0;
+	while (str[++i])
+		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\v' && str[i] != '\r' && \
+			(str[i + 1] == ' ' || str[i + 1] == '\t' || str[i + 1] == '\v' || str[i + 1] == '\r' || str[i + 1] == '\0'))
+			j++;
+	return (j);
+}
+
+char		**line_split(char const *s)
+{
+	char	**dst;
+	int		len;
+	int		i;
+	int		j;
+
+	if (!s)
+		return (0);
+	dst = (char**)malloc(sizeof(char*) * (line_wordcount((char*)s) + 1));
+	if (!dst)
+		return (0);
+	i = -1;
+	j = 0;
+	len = 0;
+	while (s[++i])
+	{
+		if (s[i] != ' ' && s[i] != '\t' && s[i] != '\v' && s[i] != '\r')
+			len++;
+		if (s[i] !=  ' ' && s[i] != '\t' && s[i] != '\v' && s[i] != '\r' \
+			&& (s[i + 1] == ' ' || s[i + 1] == '\t' || s[i + 1] == '\v' || s[i + 1] == '\r' || s[i + 1] == '\0'))
+		{
+			dst[j++] = ft_strsub(s, i - len + 1, len);
+			len = 0;
+		}
+	}
+	dst[j] = 0;
+	return (dst);
+}
+
